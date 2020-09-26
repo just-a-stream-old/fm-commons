14732096adc01e9a8ccb06c4844f08708fd33124
keirbailey <keirinvestment@gmail.com>
2020-09-26 20:54:41 +0100

    Updated fmp balance sheets dto to have @Data

--- src/main/java/finance/modelling/fmcommons/data/schema/fmp/dto/FmpBalanceSheetsDTO.java
+++ src/main/java/finance/modelling/fmcommons/data/schema/fmp/dto/FmpBalanceSheetsDTO.java
@@ -1,12 +1,10 @@
 package finance.modelling.fmcommons.data.schema.fmp.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
-@Builder
 public class FmpBalanceSheetsDTO {
     private String symbol;
     private List<FmpBalanceSheetDTO> balanceSheets;
