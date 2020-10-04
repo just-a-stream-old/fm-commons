7630dd4344cebcfb6b84ac59ca6b340c557d4588
keirbailey <keirinvestment@gmail.com>
2020-10-04 14:49:02 +0100

    Fixed income statement data type bug

--- src/main/java/finance/modelling/fmcommons/data/schema/eod/dto/EodTickerTimeSeriesDTO.java
+++ src/main/java/finance/modelling/fmcommons/data/schema/eod/dto/EodTickerTimeSeriesDTO.java
@@ -1,12 +1,12 @@
 package finance.modelling.fmcommons.data.schema.eod.dto;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
-@Builder
+@AllArgsConstructor
 public class EodTickerTimeSeriesDTO {
     private String symbol;
     private List<EodDateOHLCAVDTO> timeSeries;
